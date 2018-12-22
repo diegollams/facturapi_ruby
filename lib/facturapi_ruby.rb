a8e130aef3fac2f044eaedfabea042c2427b5d08
Diego Llamas Velasco <diegollams@gmail.com>
2018-12-22 14:09:12 -0600

    Add configuration feature

--- lib/facturapi_ruby.rb
+++ lib/facturapi_ruby.rb
@@ -1,5 +1,20 @@
 require "facturapi_ruby/version"
 
 module FacturapiRuby
-  # Your code goes here...
+
+  
+
+  # Configuration
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :api_key
+
+    def initialize
+      @api_key = ''
+    end
+  end
 end
