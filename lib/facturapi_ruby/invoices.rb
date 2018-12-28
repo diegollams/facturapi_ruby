f664a16bbb74db987a5c5344ea79636a3745dfac
Diego Llamas Velasco <diegollams@gmail.com>
2018-12-27 22:07:52 -0600

    Module invoces

--- lib/facturapi_ruby/invoices.rb
+++ lib/facturapi_ruby/invoices.rb
@@ -2,11 +2,13 @@ require 'http/client'
 
 module FacturapiRuby
   module Invoices
-    def create(options)
-      BaseHttp.post(
-        endpoint:    '/invoices',
-        api_options: options
-      )
+    class << self
+      def create(options)
+        BaseHttp.post(
+          endpoint:    '/invoices',
+          api_options: options
+        )
+      end
     end
   end
 end
